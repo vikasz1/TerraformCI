0139f9061231043626798d1ca90743f079706c7d
Vikas Maury <mauryvikas12345@gmail.com>
2026-05-23 23:28:22 +0530

    fix format

--- main.tf
+++ main.tf
@@ -3,27 +3,27 @@ terraform {
 
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~>3.43.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {}
-    skip_provider_registration = true
+  features {}
+  skip_provider_registration = true
 }
 
 
 resource "azurerm_resource_group" "rg" {
-  name = "TerraformLearn"
+  name     = "TerraformLearn"
   location = "Southeast Asia"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name = "savikas87907"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  account_tier = "Standard"
+  name                     = "savikas87907"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
   account_replication_type = "RAGRS"
 }
